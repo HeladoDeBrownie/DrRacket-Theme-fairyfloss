bc4884d2a839548b6111628925d0b572af0e6490
helado de brownie <helado.de.brownie@gmail.com>
2019-12-28 22:12:43 -0500

    remove final unused color; reorganize, reformat, and comment

--- info.rkt
+++ info.rkt
@@ -1,45 +1,47 @@
 #lang info
 
-(define background        #( 90  84 117))
-(define foreground        #(197 163 255))
-(define paren-match-color #(113 103 153))
-(define comment           #(230 192   0))
-(define string            #(255 234   0))
-(define constant          #(255 133 127))
-(define name              #(248 248 242))
-(define keyword           #(255 184 209))
-(define imported          #(194 255 223))
-(define error             #(249  38 114))
-(define warning           #(174 129 255))
-(define highlighted       #(248 248 240))
-(define arrow             #(255 243  82))
-(define matching          #(128 119 168))
+;# Colors
 
-(define unused-color #(168 117 123))
+(define basic-foreground     #(248 248 242))
+(define basic-background     #( 90  84 117))
+(define constant             #(255 133 127))
+(define string               #(255 234   0))
+(define keyword              #(255 184 209))
+(define parenthesis          #(197 163 255))
+(define comment              #(230 192   0))
+(define error                #(249  38 114))
+(define warning              #(174 129 255))
+(define highlighted          #(248 248 240))
+(define imported             #(194 255 223))
+(define paren-match-color    #(113 103 153))
+(define matching-identifiers #(128 119 168))
+(define arrow                #(255 243  82))
+
+;# Colors Table
 
 (define colors `(; Colors
-                 (drracket:syncheck:document-identifier    ,background)
-                 (drracket:syncheck:matching-identifiers   ,matching)
-                 (drracket:syncheck:tail-arrow             ,arrow)
-                 (drracket:syncheck:template-arrow         ,arrow)
-                 (drracket:syncheck:untacked               ,arrow)
-                 (drracket:syncheck:unused-identifier      ,warning)
-                 (drracket:syncheck:var-arrow              ,arrow)
-                 (framework:basic-canvas-background        ,background)
-                 (framework:default-text-color             ,foreground)
-                 (framework:misspelled-text-color          ,error)
-                 (framework:paren-match-color              ,paren-match-color)
+                 (drracket:syncheck:document-identifier  ,basic-background)
+                 (drracket:syncheck:matching-identifiers ,matching-identifiers)
+                 (drracket:syncheck:tail-arrow           ,arrow)
+                 (drracket:syncheck:template-arrow       ,arrow)
+                 (drracket:syncheck:untacked             ,arrow)
+                 (drracket:syncheck:unused-identifier    ,warning)
+                 (drracket:syncheck:var-arrow            ,arrow)
+                 (framework:basic-canvas-background      ,basic-background)
+                 (framework:default-text-color           ,basic-foreground)
+                 (framework:misspelled-text-color        ,error)
+                 (framework:paren-match-color            ,paren-match-color)
 
                  ; Styles
                  (drracket:check-syntax:both-obligation-style-pref  ,comment)
                  (drracket:check-syntax:free-variable               ,highlighted
                                                                     #s(background ,warning))
                  (drracket:check-syntax:imported                    ,imported)
-                 (drracket:check-syntax:lexically-bound             ,name)
-                 (drracket:check-syntax:my-obligation-style-pref    ,name)
+                 (drracket:check-syntax:lexically-bound             ,basic-foreground)
+                 (drracket:check-syntax:my-obligation-style-pref    ,basic-foreground)
                  (drracket:check-syntax:set!d                       ,error)
                  (drracket:check-syntax:their-obligation-style-pref ,imported)
-                 (drracket:check-syntax:unk-obligation-style-pref   ,foreground)
+                 (drracket:check-syntax:unk-obligation-style-pref   ,basic-foreground)
                  (drracket:check-syntax:unused-require              ,highlighted
                                                                     #s(background ,warning))
                  (drracket:read-eval-print-loop:error-color         ,highlighted
@@ -52,17 +54,19 @@
                                                                     #s(background ,error))
                  (framework:syntax-color:scheme:hash-colon-keyword  ,keyword)
                  (framework:syntax-color:scheme:keyword             ,keyword)
-                 (framework:syntax-color:scheme:other               ,foreground)
-                 (framework:syntax-color:scheme:parenthesis         ,foreground)
+                 (framework:syntax-color:scheme:other               ,basic-foreground)
+                 (framework:syntax-color:scheme:parenthesis         ,parenthesis)
                  (framework:syntax-color:scheme:string              ,string)
-                 (framework:syntax-color:scheme:symbol              ,name)
-                 (framework:syntax-color:scheme:text                ,foreground)
+                 (framework:syntax-color:scheme:symbol              ,basic-foreground)
+                 (framework:syntax-color:scheme:text                ,basic-foreground)
                  (plt:htdp:test-coverage-off                        ,highlighted
                                                                     #s(background ,error))
-                 (plt:htdp:test-coverage-on                         ,foreground)
+                 (plt:htdp:test-coverage-on                         ,basic-foreground)
                  (plt:module-language:test-coverage-off             ,highlighted
                                                                     #s(background ,error))
-                 (plt:module-language:test-coverage-on              ,foreground)))
+                 (plt:module-language:test-coverage-on              ,basic-foreground)))
+
+;# Configuration
 
 (define deps '("base"))
 (define framework:color-schemes `(#hash((name . "fairyfloss")
